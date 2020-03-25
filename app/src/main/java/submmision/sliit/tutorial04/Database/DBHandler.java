1238d4489b69dcbd2adee958f4f686bb0b7a7f15
alvinz97 <it18211306@my.sliit.lk>
2020-03-26 00:03:12 +0530

    Implement insert, delete, update methodes

--- app/src/main/java/submmision/sliit/tutorial04/Database/DBHandler.java
+++ app/src/main/java/submmision/sliit/tutorial04/Database/DBHandler.java
@@ -2,8 +2,12 @@ package submmision.sliit.tutorial04.Database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHandler extends SQLiteOpenHelper {
     public static final String DATABASE_NAME ="UserInfo.db";
@@ -36,5 +40,45 @@ public class DBHandler extends SQLiteOpenHelper {
         values.put(UserMaster.Users.COLUMN_NAME_PASSWORD, password);
 
         long newRowId = db.insert(UserMaster.Users.TABLE_NAME, null, values);
+    }
+
+    public List readAllInfo () {
+        SQLiteDatabase db = getReadableDatabase();
+
+        String[] projection = {
+                UserMaster.Users._ID,
+                UserMaster.Users.COLUMN_NAME_USERNAME,
+                UserMaster.Users.COLUMN_NAME_PASSWORD
+        };
+
+        String sortOrder = UserMaster.Users.COLUMN_NAME_USERNAME + " DESC";
+
+        Cursor cursor = db.query(UserMaster.Users.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                sortOrder
+        );
+
+        List userNames = new ArrayList<>();
+        List passwords = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            String username = cursor.getString(cursor.getColumnIndexOrThrow(UserMaster.Users.COLUMN_NAME_USERNAME));
+            String password = cursor.getString(cursor.getColumnIndexOrThrow(UserMaster.Users.COLUMN_NAME_PASSWORD));
+            userNames.add(username);
+            passwords.add(password);
+        }
+        cursor.close();
+        return userNames;
+    }
+
+    public void deleteInfo(String userName) {
+        SQLiteDatabase db = getReadableDatabase();
+        String selection = UserMaster.Users.COLUMN_NAME_USERNAME + " LIKE ?";
+        String[] selectionArgs = {userName};
+        db.delete(UserMaster.Users.TABLE_NAME, selection, selectionArgs);
     }
 }
